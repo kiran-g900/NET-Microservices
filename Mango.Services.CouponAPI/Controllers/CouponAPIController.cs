25b0804af31f3559071fa86c82d39cb5a51dd83a
kiran-g900 <115627096+kiran-g900@users.noreply.github.com>
2025-09-20 12:39:27 +0530

    Add project files.

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,131 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private readonly IMapper _mapper;
+        private readonly ResponseDto _response;
+
+        public CouponAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                _response.Result = _mapper.Map<IEnumerable<CouponDto>>(objList);
+                return Ok(_response);
+            }
+            catch (Exception ex) 
+            { 
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                //return StatusCode(500, "Internal server error");
+                return StatusCode(500, _response.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.FirstOrDefault(c => c.CouponId == id);                
+                if (coupon == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.Message = $"Coupon with ID {id} not found.";
+                    return NotFound(_response);
+                }                
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                return StatusCode(500, _response.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public IActionResult GetByCode(string code)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.FirstOrDefault(c => c.CouponCode.ToLower() == code.ToLower());
+                if (coupon == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.Message = $"Coupon with code {code} not found.";
+                    return NotFound(_response);
+                }
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+                return StatusCode(500, _response.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CouponDto couponDto)
+        {
+            Coupon coupon = _mapper.Map<Coupon>(couponDto);
+            _db.Coupons.Add(coupon);
+            _db.SaveChanges();
+
+            _response.Result = _mapper.Map<CouponDto>(coupon);
+            return Ok(_response);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] CouponDto couponDto)
+        {
+            Coupon coupon = _mapper.Map<Coupon>(couponDto);
+            _db.Coupons.Update(coupon);
+            _db.SaveChanges();
+
+            _response.Result = _mapper.Map<CouponDto>(coupon);
+            return Ok(_response);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            Coupon coupon = _db.Coupons.FirstOrDefault(c => c.CouponId == id);
+            if(coupon == null)
+            {
+                _response.IsSuccess = false;
+                _response.Message = "No coupon found";
+                return NotFound(_response);
+            }
+            _db.Coupons.Remove(coupon);
+            _db.SaveChanges();
+
+            _response.IsSuccess = true;
+            _response.Message = "Coupon deleted successfully";
+            return Ok(_response);
+        }
+    }
+}
---
+++ Mango.Services.CouponAPI/Program.cs
@@ -0,0 +1,57 @@
+using AutoMapper;
+using Mango.Services.CouponAPI;
+using Mango.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(option => {
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Coupon", Version = "v1" });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+ApplyMigration();
+app.Run();
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        
+        if(_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
